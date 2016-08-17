51f7a9ed1acd572a7c09f460baa5f61cb96fc7fe
Yu Jin <dearrrfish@gmail.com>
2016-08-17 14:08:35 -0700

    Fix build script when dist folder doesn't exist.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 for file in ./src/*.js; do
+    mkdir -p ./dist
     (echo "#!/usr/bin/env osascript -l JavaScript"; ./node_modules/.bin/rollup -c $1 -i $file ) > "./dist/$(basename $file)";
     chmod +x "./dist/$(basename $file)";
 done
